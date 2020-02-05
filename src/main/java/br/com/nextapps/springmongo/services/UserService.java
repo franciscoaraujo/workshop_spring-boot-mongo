a76566a84292bc4a22b1e93166a5cd0df3d27d99
foaj <foaj@BRPC003901.gft.com>
2020-02-05 12:46:37 -0200

    Obtendo um usuário por id e adicionando excecoes personalizadas

--- src/main/java/br/com/nextapps/springmongo/services/UserService.java
+++ src/main/java/br/com/nextapps/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.nextapps.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.nextapps.springmongo.domain.User;
 import br.com.nextapps.springmongo.repositories.UserRepository;
+import br.com.nextapps.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	
 	public List<User>findAll(){
 		return repo.findAll();
+	}
+	
+	public User findaById(String id) {
+		Optional<User> user =  repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/br/com/nextapps/springmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.nextapps.springmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
