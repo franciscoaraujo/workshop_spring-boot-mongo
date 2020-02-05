81825256d0762f7b452b246a2124e1dcf1126950
foaj <foaj@BRPC003901.gft.com>
2020-02-05 16:43:26 -0200

    Obtendo um post por id

---
+++ src/main/java/br/com/nextapps/springmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.nextapps.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.nextapps.springmongo.domain.Post;
+import br.com.nextapps.springmongo.repositories.PostRepository;
+import br.com.nextapps.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
