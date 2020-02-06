ef0ae3bd050b26d7aa212e0c931f24dcbdd7144d
foaj <foaj@BRPC003901.gft.com>
2020-02-06 09:34:27 -0200

    Consulta simples com @Query

--- src/main/java/br/com/nextapps/springmongo/repositories/PostRepository.java
+++ src/main/java/br/com/nextapps/springmongo/repositories/PostRepository.java
@@ -1,9 +1,18 @@
 package br.com.nextapps.springmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import br.com.nextapps.springmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
