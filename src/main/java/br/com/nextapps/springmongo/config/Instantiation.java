a480c6ff12904c24c7c331565b04ea128aeea65d
foaj <foaj@BRPC003901.gft.com>
2020-02-06 10:37:03 -0200

    Consulta com vários critérios

--- src/main/java/br/com/nextapps/springmongo/config/Instantiation.java
+++ src/main/java/br/com/nextapps/springmongo/config/Instantiation.java
@@ -4,8 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
 
-import javax.xml.stream.events.Comment;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
